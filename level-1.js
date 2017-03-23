e91dff1313d8d0c4f7ce605501b526d1a7955a54
Maksim Mihalchuk <mmikhalchuk@users.noreply.github.com>
2017-03-23 23:48:24 +0200

    Level number 1

---
+++ level-1.js
@@ -0,0 +1,119 @@
+//block number 1
+var a = 3;
+console.log(a);
+
+//block number 2
+var a = 10;
+var b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+//block number 3
+var c = 15;
+var d = 2;
+var result = c + d;
+console.log(result);
+
+//block number 4
+var a = 10;
+var b = 2;
+var c = 5;
+console.log(a + b + c);
+
+//block number 5
+var a = 17;
+var b = 10;
+var c = a - b;
+var d = 7;
+var result = c + d;
+console.log(result);
+
+//block number 6
+var text = "Привет, Мир!";
+console.log(text);
+
+//block number 7
+var text1 = "Привет, ";
+var text2 = "Мир!";
+console.log(text1 + text2);
+
+//block number 8
+var name = "Maks";
+console.log("Привет, " + name);
+
+//block number 9
+var age = "14";
+console.log("Мне " + age + " лет!");
+
+//block number 10
+var name = prompt("Напишите ваше имя");
+console.log("Ваше имя " + name);
+
+//block number 11
+var number = prompt("Напишите любое число");
+console.log(number * number);
+
+//block number 12
+var text = "Мама мыла раму!";
+console.log(text[6]);
+console.log(text[0]);
+console.log(text[2]);
+console.log(text[5]);
+console.log(text[12]);
+console.log(text[1]);
+console.log(text[3]);
+console.log(text[8]);
+console.log(text[11]);
+console.log(text[13]);
+console.log(text[14]);
+
+//block number 13
+var game = "Путешественник";
+console.log(game[2] + game[3] + game[5] + game[6]);
+
+//block number 14
+var sec = 60;
+var hour = sec * sec;
+var day = hour * 24;
+var month = day * 30;
+console.log(sec + " " + hour + " " + day + " " + month);
+
+//block number 15
+var sec = "01";
+var min = "02";
+var hour = "03";
+console.log(hour + ":" + min + ":" + sec);
+
+//block number 16
+var number = 15;
+console.log(number * number);
+
+//block number 17
+var t = 25;
+console.log(5 * (t - 32) / 9);
+
+//block number 18
+var test = 47;
+test += 7;
+test -= 18;
+test *= 10;
+console.log(test);
+
+//block number 19
+var test = 10;
+test++;
+test++;
+test--;
+console.log(test);
+
+//block number 20
+var test = 10;
+test += 7;
+test++;
+test--;
+test += 12;
+test *= 7;
+test -= 15;
+console.log(test);
