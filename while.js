d849a11b3e46ebf443d29f8ec496878859e31748
Maks Mikhalchuk <mmikhalchuk@users.noreply.github.com>
2017-03-25 23:47:49 +0200

    level-4
    Create level number 4

---
+++ while.js
@@ -0,0 +1,177 @@
+//block number 1.1
+var a = 0;
+while(a <= 100) {
+    console.log(a);
+    a++;
+}
+//block number 1.2
+for(var b = 0; b <= 100; b++) {
+    console.log(b);
+}
+
+//block number 2.1
+var c = 11;
+while(c <= 33) {
+    console.log(c);
+    c++;
+}
+
+//block number 2.2
+for(var d = 11; d <= 33; d++) {
+    console.log(d);
+}
+
+//block number 3.1
+var e = 0;
+while(e <= 100) {
+    console.log(e);
+    e += 2;
+}
+
+//block number 3.2
+for(var f = 0; f <= 100; f += 2) {
+    console.log(f);
+}
+
+//block number 4.1
+var g = 1;
+while(g <= 100) {
+    console.log(g);
+    g += 2;
+}
+
+//block number 4.2
+for(var i = 1; i <= 100; i += 2) {
+    console.log(i);
+}
+
+//block number 5
+var n1 = 1000;
+var num1 = 0;
+while(n1 > 50) {
+    console.log(n1);
+    n1 /= 2;
+    num1++;
+}
+console.log(num1);
+
+//block number 5
+var num2 = 0;
+for(var n2 = 1000; n2 > 50; n2 /= 2) {
+    console.log(n2);
+    num2++;
+}
+console.log(num2);
+
+//block number 6
+var arr1 = [1, 20, 15, 17, 24, 35];
+for(var i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+}
+
+//block number 7
+var arr2 = [1, 20, 15, 17, 24, 35];
+var result = 0;
+for(var i = 0; i < arr2.length; i++) {
+    result += arr2[i];
+}
+console.log(result);
+
+//block number 8
+var obj1 = {
+    green: 'зеленый',
+    red: 'красный',
+    blue: 'голубой'
+};
+for(var key1 in obj1) {
+    console.log(`${key1}: ${obj1[key1]}`);
+}
+
+//block number 9
+var obj2 = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400'
+};
+for(var key2 in obj2) {
+    console.log(`${key2} - зарплата ${obj2[key2]} долларов`);
+}
+
+//block number 10
+var arr3 = [2, 5, 9, 15, 0, 4];
+for(var k = 0; k < arr3.length; k++) {
+    if(arr3[k] > 3 && arr3[k] < 10) {
+        console.log(arr3[k]);
+    }
+}
+
+//block number 11
+var arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var str = '-';
+for(var f = 0; f < arr4.length; f++) {
+    str += `${arr4[f]}-`;
+}
+console.log(str);
+
+//block number 12
+var arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for(var t = 0; t < arr5.length; t++) {
+    if(arr5[t] % 3 == 0) {
+        console.log(`${arr5[t-2]} ${arr5[t-1]} ${arr5[t]}`);
+    }
+}
+
+//block number 13
+var arr6 = ['Ян', 'Фв', 'Мр', 'Ап'];
+var month = 3;
+for(var s = 0; s < arr6.length; s++) {
+    if(s == (month - 1)) {
+        console.log(arr6[s].toUpperCase());
+    } else {
+        console.log(arr6[s]);
+    }
+}
+
+//block number 14
+var arr7 = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+for(var m = 0; m < arr7.length; m++) {
+    if(arr7[m] == 'Сб' || arr7[m] == 'Вс') {
+        console.log(arr7[m].toUpperCase());
+    } else {
+        console.log(arr7[m]);
+    }
+}
+
+//block number 15
+var arr8 = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+var day = 6;
+for(var h = 0; h < arr8.length; h++) {
+    if(h == (day - 1)) {
+        console.log(arr8[h].toUpperCase());
+    } else {
+        console.log(arr8[h]);
+    }
+}
+
+//block number 16
+var str4 = '';
+for(var x = 1; x <= 20; x++) {
+    str4 += 'x';
+    console.log(str4);
+}
+
+//block number 17
+for(var z = 1; z <= 9; z++) {
+    var str5 = '';
+    for(var q = 1; q <= z; q++) {
+        str5 += z;
+    }
+    console.log(str5);
+}
+
+//block number 18
+var str7 = '';
+for(var j = 1; j <= 20; j++) {
+    str7 += 'xx';
+    console.log(str7);
+}
